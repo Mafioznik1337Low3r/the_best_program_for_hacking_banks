5b773ba537995c9a37aa4b7f0f6301c5f1b5ebbd
Mafioznik <45047507+Mafioznik1337Low3r@users.noreply.github.com>
2018-12-06 18:38:06 +0300

    Add files via upload

---
+++ best_programm_ever.cpp
@@ -0,0 +1,50 @@
+                                                                                 
+                                                                                 
+   SSSSSSSSSSSSSSS IIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTTIIIIIIIIII   SSSSSSSSSSSSSSS 
+ SS:::::::::::::::SI::::::::IT:::::::::::::::::::::TI::::::::I SS:::::::::::::::S
+S:::::SSSSSS::::::SI::::::::IT:::::::::::::::::::::TI::::::::IS:::::SSSSSS::::::S
+S:::::S     SSSSSSSII::::::IIT:::::TT:::::::TT:::::TII::::::IIS:::::S     SSSSSSS
+S:::::S              I::::I  TTTTTT  T:::::T  TTTTTT  I::::I  S:::::S            
+S:::::S              I::::I          T:::::T          I::::I  S:::::S            
+ S::::SSSS           I::::I          T:::::T          I::::I   S::::SSSS         
+  SS::::::SSSSS      I::::I          T:::::T          I::::I    SS::::::SSSSS    
+    SSS::::::::SS    I::::I          T:::::T          I::::I      SSS::::::::SS  
+       SSSSSS::::S   I::::I          T:::::T          I::::I         SSSSSS::::S 
+            S:::::S  I::::I          T:::::T          I::::I              S:::::S
+            S:::::S  I::::I          T:::::T          I::::I              S:::::S
+SSSSSSS     S:::::SII::::::II      TT:::::::TT      II::::::IISSSSSSS     S:::::S
+S::::::SSSSSS:::::SI::::::::I      T:::::::::T      I::::::::IS::::::SSSSSS:::::S
+S:::::::::::::::SS I::::::::I      T:::::::::T      I::::::::IS:::::::::::::::SS 
+ SSSSSSSSSSSSSSS   IIIIIIIIII      TTTTTTTTTTT      IIIIIIIIII SSSSSSSSSSSSSSS   
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+        CCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTFFFFFFFFFFFFFFFFFFFFFF               
+     CCC::::::::::::CT:::::::::::::::::::::TF::::::::::::::::::::F               
+   CC:::::::::::::::CT:::::::::::::::::::::TF::::::::::::::::::::F               
+  C:::::CCCCCCCC::::CT:::::TT:::::::TT:::::TFF::::::FFFFFFFFF::::F               
+ C:::::C       CCCCCCTTTTTT  T:::::T  TTTTTT  F:::::F       FFFFFF               
+C:::::C                      T:::::T          F:::::F                            
+C:::::C                      T:::::T          F::::::FFFFFFFFFF                  
+C:::::C                      T:::::T          F:::::::::::::::F                  
+C:::::C                      T:::::T          F:::::::::::::::F                  
+C:::::C                      T:::::T          F::::::FFFFFFFFFF                  
+C:::::C                      T:::::T          F:::::F                            
+ C:::::C       CCCCCC        T:::::T          F:::::F                            
+  C:::::CCCCCCCC::::C      TT:::::::TT      FF:::::::FF                          
+   CC:::::::::::::::C      T:::::::::T      F::::::::FF                          
+     CCC::::::::::::C      T:::::::::T      F::::::::FF                          
+        CCCCCCCCCCCCC      TTTTTTTTTTT      FFFFFFFFFFF                          
+                                                                                 
+                                                                                 
+    SITIS{mafioznik_was_here}                                                                             
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
